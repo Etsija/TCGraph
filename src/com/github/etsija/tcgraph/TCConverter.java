0dd0cc9dc6abafb395758051f800636197e13be5
Jyrki Keisala <jyrki.keisala@gmail.com>
2013-01-10 12:25:44 +0200

    Drop GUI altogether, replace with intelligent command-line arguments handling.

--- src/com/github/etsija/tcgraph/TCConverter.java
+++ src/com/github/etsija/tcgraph/TCConverter.java
@@ -10,14 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.zip.InflaterInputStream;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-
 public class TCConverter {
 	
 	// This class represents an edge in the routing graph
@@ -76,188 +68,197 @@ public class TCConverter {
 	// Main
 	public static void main(String[] args) {
 		
-		boolean useGUI = false;
+		int connBeforeRemoval = 0;
+		int connAfterRemoval = 0;
+		boolean labelIntersections = false;
+		boolean debug = false;
+		File sourceFile = null;
+		File textFile = null;
+		File dotFile = null;
 		
-		if (args.length == 0) {
-			useGUI = true;
-		}
+		if (args.length < 3) {
+
+			System.out.println("Usage: <destinations.dat> <destinations.txt> <destinations.dot> -label -debug");
+			System.out.println("  <destinations.dat> = (mandatory) the TrainCarts input file");
+			System.out.println("  <destinations.txt> = (mandatory) informational output file");
+			System.out.println("  <destinations.dot> = (mandatory) dotfile to input to DOT tool");
+			System.out.println("              -label = (optional) label intersections with \"JN\" ");
+			System.out.println("              -debug = (optional) print out some debug info");
+			System.out.println();
+			System.out.println("Note: args can be in any order as long as the filenames include");
+			System.out.println(".dat, .txt, .dot, respectively.  For example this is still valid:");
+			System.out.println("> tcgraph destinations.txt -debug destinations.dot -label destinations.dat");
+			System.exit(0);
+
+		} else {
 		
-		// Create the JFileChooser
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setDialogTitle("Select the destinations.dat file to open");
-		fileChooser.setCurrentDirectory(new File("."));
-		
-		// Create an accessory panel for options and attach it to JFileChooser
-		JCheckBox cbIntersections = new JCheckBox("Label intersections");
-		JPanel optionsAccessory = new JPanel();
-		Border border = BorderFactory.createTitledBorder("Options");
-	    optionsAccessory.setBorder(border);
-		optionsAccessory.setLayout(new BoxLayout(optionsAccessory, BoxLayout.Y_AXIS));
-		optionsAccessory.add(cbIntersections);
-		fileChooser.setAccessory(optionsAccessory);
-		
-		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-			File sourceFile = fileChooser.getSelectedFile();
-			String sourceBase = sourceFile.getName().split("\\.")[0];
+			// Handle arguments
+			for (String s: args) {
+				if (s.contains(".dat"))
+					sourceFile = new File(s);
+				else if (s.contains(".txt"))
+					textFile = new File(s);
+				else if (s.contains(".dot"))
+					dotFile = new File(s);
+				else if (s.equals("-label"))
+					labelIntersections = true;
+				else if (s.equals("-debug"))
+					debug = true;			
+			}
 			
-			fileChooser.setDialogTitle("Select the file to save the converted text to");
-			fileChooser.setSelectedFile(new File(sourceFile.getParentFile(), sourceBase + ".txt"));
-			
-			if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-				File destFile = fileChooser.getSelectedFile();
-				
-				// These are the variables that are used when handling the options while converting
-				boolean labelIntersections = cbIntersections.isSelected();
+			try {
+				DataInputStream stream = new DataInputStream(new InflaterInputStream(new FileInputStream(sourceFile)));
+				PathNode[] nodes;
 				
 				try {
-					DataInputStream stream = new DataInputStream(new InflaterInputStream(new FileInputStream(sourceFile)));
-					PathNode[] nodes;
-				
-					try {
-						nodes = new PathNode[stream.readInt()];
-						HashMap<String, PathNode> nodesMap = new HashMap<String, PathNode>(nodes.length);
+					nodes = new PathNode[stream.readInt()];
+					HashMap<String, PathNode> nodesMap = new HashMap<String, PathNode>(nodes.length);
 						
-						// Read all node information
-						int j = 1;
-						for (int i = 0; i < nodes.length; i++) {
-							nodes[i] = new PathNode();
-							nodes[i].name = stream.readUTF();
-							nodes[i].world = stream.readUTF();
-							nodes[i].x = stream.readInt();
-							nodes[i].y = stream.readInt();
-							nodes[i].z = stream.readInt();
+					// Read all node information
+					int j = 1;
+					for (int i = 0; i < nodes.length; i++) {
+						nodes[i] = new PathNode();
+						nodes[i].name = stream.readUTF();
+						nodes[i].world = stream.readUTF();
+						nodes[i].x = stream.readInt();
+						nodes[i].y = stream.readInt();
+						nodes[i].z = stream.readInt();
 						
-							if (nodes[i].name.isEmpty()) {
-								nodes[i].name = "JN" + Integer.toString(j);
-								j++;
-							}
-							nodesMap.put(nodes[i].name, nodes[i]);
+						if (nodes[i].name.isEmpty()) {
+							nodes[i].name = "JN" + Integer.toString(j);
+							j++;
 						}
-
-						// Read all connections
-						for (PathNode node : nodes) {
-							
-							PathConnection conn;
-							int ncount = stream.readInt();
-							
-							System.out.print(node.name + " conn: " + ncount);
-							
-							for (int i = 0; i < ncount; i++) {
-								conn = new PathConnection(stream, nodes[stream.readInt()]);
-								node.connections.add(conn);
-							}							
-						}
-						
-					} finally {
-						stream.close();
+						nodesMap.put(nodes[i].name, nodes[i]);
 					}
 
-					// Find and mark the two-way connections so they are not drawn twice in the graph
-					int removedNodes = markTwoWay(nodes);
-					
+					// Read all connections
 					for (PathNode node : nodes) {
-						for (PathConnection conn : node.connections) {
-							if (conn.shouldBeListed)
+							
+						PathConnection conn;
+						int ncount = stream.readInt();
+						connBeforeRemoval += ncount;
+						
+						if (debug) {
+							System.out.println(node.name + " conn: " + ncount);
+						}
+							
+						for (int i = 0; i < ncount; i++) {
+							conn = new PathConnection(stream, nodes[stream.readInt()]);
+							node.connections.add(conn);
+						}							
+					}
+						
+				} finally {
+					stream.close();
+				}
+
+				// Find and mark the two-way connections so they are not drawn twice in the graph
+				int removedConn = markTwoWay(nodes);
+					
+				for (PathNode node : nodes) {
+					for (PathConnection conn : node.connections) {
+						if (conn.shouldBeListed) {
+							if (debug) {
 								System.out.println(node.name + " -> " + conn.destination.name 
 										+ ", istwoway=" + conn.isTwoway);
+							}
+							connAfterRemoval += 1;
 						}
 					}
-					System.out.println(removedNodes + " nodes removed.");
-					
-					// Start writing this info
-					BufferedWriter  writer = new BufferedWriter(new FileWriter(destFile));
-					String destBase = destFile.getAbsolutePath().split("\\.")[0];
-					BufferedWriter dwriter = new BufferedWriter(new FileWriter(destBase + ".dot"));
-
-					try {
-						dwriter.write("graph PathFinding {");
-						dwriter.newLine();
-						
-						dwriter.newLine();
-						dwriter.write("    node [width=0, height=0, margin=0, fontname=Helvetica, fontsize=14];");
-						dwriter.newLine();
-						dwriter.newLine();
-						int totalTracks = 0;
-						for (PathNode node : nodes) {
-							writer.write("name: ");
-							writer.write(node.name);
-							writer.newLine();
-							
-							dwriter.write("    \"" + node.name + "\" [");
-							// Flip the z coordinate to make the graphics roughly respect the
-							// real directions (n,e,s,w)
-							dwriter.write("pos=\"" + node.x + "," + -node.z + "\", ");
-							
-							// Switches (junctions) represented as points or texts, depending on user's choice
-							if (node.name.startsWith("JN")) {
-								if (labelIntersections)
-									dwriter.write("shape=plaintext, fontsize=10");
-								else
-									dwriter.write("shape=point, width=0.1, height=0.1");
-							// Real destinations represented as yellow boxes
-							} else {
-								dwriter.write("style=filled, " +
-										      "shape=ellipse, " +
-										      "fillcolor=yellow, " +
-										      "margin=0.08");
-							}
-							dwriter.write("];");
-							dwriter.newLine();
-							
-							writer.write("position: ");
-							writer.write(node.getLoc());
-							writer.newLine();
-							writer.write("connections (" + node.connections.size() + "):");
-							writer.newLine();
-							
-							for (PathConnection conn : node.connections) {					
-								writer.write("- ");
-								writer.write(conn.destination.name);
-								writer.newLine();
-								
-								if (conn.shouldBeListed) {
-									if (conn.isTwoway) {
-										dwriter.write("    \"" + node.name + "\" -- \"" + conn.destination.name + "\" [" 
-											      + "label=" + conn.distance + "];");
-									} else {
-											dwriter.write("    \"" + node.name + "\" -- \"" + conn.destination.name + "\" ["
-												  + "dir=\"forward\", "
-										          + "label=" + conn.distance + ", "
-										          + "color=red];");
-									}
-									dwriter.newLine();
-									totalTracks = totalTracks + conn.distance;
-								}
-								
-								writer.write("    position: ");
-								writer.write(conn.destination.getLoc());
-								writer.newLine();
-								writer.write("    direction: ");
-								writer.write(conn.direction);
-								writer.newLine();
-								writer.write("    distance: ");
-								writer.write(Integer.toString(conn.distance));
-								writer.newLine();
-							}
-							writer.newLine();
-							dwriter.newLine();
-						}
-						System.out.println("Total number of tracks: " + totalTracks);
-						dwriter.write("}");
-						dwriter.newLine();
-					} finally {
-						writer.close();
-						dwriter.close();
-					}
-					JOptionPane.showMessageDialog(null, "The operation was successful!", "Conversion finished", JOptionPane.INFORMATION_MESSAGE);
-				} catch (Exception ex) {
-					JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage(), "Error while loading file", JOptionPane.ERROR_MESSAGE);
 				}
-				System.exit(0);
-				return;
+				System.out.println("Connections in original graph: " + connBeforeRemoval);
+				System.out.println("  Two-way connections removed: " + removedConn);
+				System.out.println("   Connections in final graph: " + connAfterRemoval);
+					
+				// Start writing this info
+				BufferedWriter  writer = new BufferedWriter(new FileWriter(textFile));
+				BufferedWriter dwriter = new BufferedWriter(new FileWriter(dotFile));
+
+				try {
+					dwriter.write("graph PathFinding {");
+					dwriter.newLine();
+						
+					dwriter.newLine();
+					dwriter.write("    node [width=0, height=0, margin=0, fontname=Helvetica, fontsize=14];");
+					dwriter.newLine();
+					dwriter.newLine();
+					int totalTracks = 0;
+					for (PathNode node : nodes) {
+						writer.write("name: ");
+						writer.write(node.name);
+						writer.newLine();
+							
+						dwriter.write("    \"" + node.name + "\" [");
+						// Flip the z coordinate to make the graphics roughly respect the
+						// real directions (n,e,s,w)
+						dwriter.write("pos=\"" + node.x + "," + -node.z + "\", ");
+							
+						// Switches (junctions) represented as points or texts, depending on user's choice
+						if (node.name.startsWith("JN")) {
+							if (labelIntersections)
+								dwriter.write("shape=plaintext, fontsize=10");
+							else
+								dwriter.write("shape=point, width=0.1, height=0.1");
+						// Real destinations represented as yellow boxes
+						} else {
+							dwriter.write("style=filled, " +
+									      "shape=ellipse, " +
+									      "fillcolor=yellow, " +
+									      "margin=0.08");
+						}
+						dwriter.write("];");
+						dwriter.newLine();
+							
+						writer.write("position: ");
+						writer.write(node.getLoc());
+						writer.newLine();
+						writer.write("connections (" + node.connections.size() + "):");
+						writer.newLine();
+							
+						for (PathConnection conn : node.connections) {					
+							writer.write("- ");
+							writer.write(conn.destination.name);
+							writer.newLine();
+								
+							if (conn.shouldBeListed) {
+								if (conn.isTwoway) {
+									dwriter.write("    \"" + node.name + "\" -- \"" + conn.destination.name + "\" [" 
+										      + "label=" + conn.distance + "];");
+								} else {
+										dwriter.write("    \"" + node.name + "\" -- \"" + conn.destination.name + "\" ["
+											  + "dir=\"forward\", "
+									          + "label=" + conn.distance + ", "
+									          + "color=red];");
+								}
+								dwriter.newLine();
+								totalTracks = totalTracks + conn.distance;
+							}
+								
+							writer.write("    position: ");
+							writer.write(conn.destination.getLoc());
+							writer.newLine();
+							writer.write("    direction: ");
+							writer.write(conn.direction);
+							writer.newLine();
+							writer.write("    distance: ");
+							writer.write(Integer.toString(conn.distance));
+							writer.newLine();
+						}
+						writer.newLine();
+						dwriter.newLine();
+					}
+					System.out.println("       Total number of tracks: " + totalTracks);
+					dwriter.write("}");
+					dwriter.newLine();
+				} finally {
+					writer.close();
+					dwriter.close();
+				}
+				System.out.println("The operation was successful!");
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
 		}
-		JOptionPane.showMessageDialog(null, "Cancelled, the program will now close.", "Aborted", JOptionPane.INFORMATION_MESSAGE);
 		System.exit(0);
 	}
 }
